78d521a57f4e7f4b52abb8e3005811f81b7c02d4
Shivraj <gj08210@umbc.edu>
2020-03-30 21:12:17 -0400

    code file created for 2D image transformation

---
+++ 2d.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define u32 unsigned int
+
+u32 N;
+
+int main()
+{
+    printf("!!!---Code File for 2D image transformation---!!!");
+}
