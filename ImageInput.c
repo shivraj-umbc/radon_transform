37c8b21e9b0d5a7db8716ad204e1ab14b715e0fb
Shivraj <gj08210@umbc.edu>
2020-04-29 19:49:42 -0400

    image_pixels to image_2D_array input code file
    image_pixels to image_2D_array input code file

---
+++ ImageInput.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dc_image.h"
+#define ABS(x)    ( (x) <= 0 ? 0-(x) : (x) )
+
+int main()
+{
+	int y,x;
+	int rows, cols, chan;
+
+    byte ***img = LoadRgb(".\\TestImage.bmp", &rows, &cols, &chan);
+    printf("img %p rows %d cols %d chan %d\n", img, rows, cols, chan);
+
+	byte **gray = malloc2d(rows, cols);
+	for (y=0; y<rows; y++){
+		for (x=0; x<cols; x++) {
+			int r = img[y][x][0];
+			int g = img[y][x][1];
+			int b = img[y][x][2];
+			gray[y][x] =  (r+g+b) / 3;
+		}
+	}
+
+    SaveGrayPng(gray, "output.png", rows, cols);
+	
+}
